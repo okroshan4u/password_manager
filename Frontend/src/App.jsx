7433cd365651ab8f5428a17de8f2d11ad0b3cf15
Roshan <okroshan4u@gmail.com>
2025-06-03 22:05:03 +0530

    deleted the unused code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,8 +4,6 @@ import Manager from './Components/Manager'
 import Footer from './Components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navbar />
